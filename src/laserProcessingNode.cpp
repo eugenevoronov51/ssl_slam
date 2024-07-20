205bfc59f325c3f856943bb2c8eecfbc2068e97c
Eugene Voronov <eugene@hmt.ai>
2024-07-20 12:54:28 +0300

    Updated

--- src/laserProcessingNode.cpp
+++ src/laserProcessingNode.cpp
@@ -134,7 +134,7 @@ int main(int argc, char **argv)
 
     laserProcessing.init(lidar_param);
 
-    ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/camera/depth/color/points", 100, velodyneHandler);
+    ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>("/l515/depth/color/points", 100, velodyneHandler);
 
     pubLaserCloudFiltered = nh.advertise<sensor_msgs::PointCloud2>("/velodyne_points_filtered", 100);
 
